c05ab46ab8afd01708ebbee3a879240fd8f76740
Ashley Rosch <jaketr00@gmail.com>
2021-03-02 20:02:28 -0600

    1.2.2

--- src/main/java/dev/zomo/acmd/strippedlog.java
+++ src/main/java/dev/zomo/acmd/strippedlog.java
@@ -15,67 +15,14 @@ public class strippedlog implements Listener {
 
             Material itemType = event.getItem().getType();
 
-            if (itemType.equals(Material.WOODEN_AXE) || itemType.equals(Material.STONE_AXE)
-                    || itemType.equals(Material.IRON_AXE) || itemType.equals(Material.GOLDEN_AXE)
-                    || itemType.equals(Material.DIAMOND_AXE) || itemType.equals(Material.NETHERITE_AXE)) {
+            if (itemType.getKey().toString().endsWith("_axe")) {
 
                 Block block = event.getClickedBlock();
 
                 Material blockType = block.getType();
 
-                boolean changed = false;
-
-                if (blockType.equals(Material.STRIPPED_ACACIA_LOG)) {
-                    block.setType(Material.ACACIA_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_ACACIA_WOOD)) {
-                    block.setType(Material.ACACIA_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_BIRCH_LOG)) {
-                    block.setType(Material.BIRCH_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_BIRCH_WOOD)) {
-                    block.setType(Material.BIRCH_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_CRIMSON_HYPHAE)) {
-                    block.setType(Material.CRIMSON_HYPHAE);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_CRIMSON_STEM)) {
-                    block.setType(Material.CRIMSON_STEM);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_DARK_OAK_LOG)) {
-                    block.setType(Material.DARK_OAK_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_DARK_OAK_WOOD)) {
-                    block.setType(Material.DARK_OAK_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_JUNGLE_LOG)) {
-                    block.setType(Material.JUNGLE_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_JUNGLE_WOOD)) {
-                    block.setType(Material.JUNGLE_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_OAK_LOG)) {
-                    block.setType(Material.OAK_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_OAK_WOOD)) {
-                    block.setType(Material.OAK_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_SPRUCE_LOG)) {
-                    block.setType(Material.SPRUCE_LOG);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_SPRUCE_WOOD)) {
-                    block.setType(Material.SPRUCE_WOOD);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_WARPED_HYPHAE)) {
-                    block.setType(Material.WARPED_HYPHAE);
-                    changed = true;
-                } else if (blockType.equals(Material.STRIPPED_WARPED_STEM)) {
-                    block.setType(Material.WARPED_STEM);
-                    changed = true;
-                }
-
-                if (changed) {
+                if (blockType.getKey().toString().startsWith("minecraft:stripped_")) {
+                    block.setType(Material.matchMaterial("minecraft:" + blockType.getKey().toString().substring(19)));
                     event.setCancelled(true);
                     event.getPlayer().playSound(block.getLocation(), "item.axe.strip", 1, 1);
                 }
